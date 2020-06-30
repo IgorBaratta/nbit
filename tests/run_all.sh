415c34813f90efb762f734951b189fb45bef8625
IgorBaratta <igorbaratta@gmail.com>
2020-06-30 17:54:51 -0300

    test multiple compilers

--- tests/run_all.sh
+++ tests/run_all.sh
@@ -1,18 +1,19 @@
 #!bin/bash
 
-# compiler="g++ clang++"
-# standard="c++11 c++14 c++17 c++2a"
-compiler="g++"
-standard="c++17"
+compiler="g++ clang++"
+standard="c++11 c++14 c++17 c++2a"
+# compiler="g++"
+# standard="c++17"
 
 export CATCH_CONFIG_FAST_COMPILE
 for cpp in $compiler
 do
     for std in $standard
     do
-        echo "Using $cpp std $std"
+        echo "==================================================="
+        echo "Using $cpp -std=$std"
         $cpp -g -std=$std tests_main.cpp -c
         $cpp -g -std=$std tests_dynamic.cpp -c
-        $cpp -g -std=$std tests_main.o tests_dynamic.o -o tests && ./tests --success
+        $cpp -g -std=$std tests_main.o tests_dynamic.o -o tests.out && ./tests.out --reporter compact --success
     done
 done
